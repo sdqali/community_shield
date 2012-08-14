1b1f605288bf39c3c0045c0c79ba099d9f6977c3
Sadique Ali <sadiqalikm@gmail.com>
2012-08-15 00:31:11 +0530

    Improved stuff.

--- js/charity_shield.js
+++ js/charity_shield.js
@@ -1,50 +1,35 @@
 function drawVis () {
-  var width = 1000;
-  var height = 600;
-  var margin = {top: 40, right: 40, bottom: 40, left: 40};
-
-  var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("class", "time chart")
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  d3.csv ("charity_shield.csv", function (data) {
+  d3.csv("charity_shield.csv", function(data) {
     data = data.filter (function (d) {
       return +d.LeaguePosition != -1;
     });
 
+    var width = 1000;
+    var height = 600;
+    var margin = {top: 40, right: 40, bottom: 40, left: 40};
+
     var xScale = d3.time.scale()
       .range([0, width - margin.left - margin.right])
-      .domain(d3.extent(data, function(d) { return d.Year; }));
+      .domain(d3.extent(data, function(d) { return +d.Year; }));
 
     var yScale = d3.scale.linear()
       .range([height - margin.top - margin.bottom, 0])
-      .domain([d3.max(data, function(d) { return +d.LeaguePosition; }), 0]);
+      .domain([d3.max(data, function(d) { return +d.LeaguePosition; }), 1]);
 
-    var xAxis = d3.svg.axis()
-      .scale(xScale)
-      .orient("bottom")
-      .tickFormat (d3.format ("d"))
-      .ticks (30)
-      .tickPadding(8);
+    var vis = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("class", "winners")
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var yAxis = d3.svg.axis()
-      .scale(yScale)
-      .orient("left")
-      .tickPadding(8);
+    var line = d3.svg.line()
+      .x(function(d,i) { return xScale(+d.Year); })
+      .y(function(d) { return yScale(+d.LeaguePosition); })
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + yScale.range()[0] + ")")
-      .call(xAxis);
+    vis.append("svg:path").attr("d", line(data));
 
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-
-    svg.selectAll ("circle")
+    vis.selectAll ("circle")
       .data (data)
       .enter ()
       .append ("circle")
@@ -61,12 +46,25 @@ function drawVis () {
         return d.Year + " : " + d.Winner + ", " + d.LeaguePosition;
       });
 
-    var g = svg.append("svg:g");
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom")
+      .tickFormat (d3.format ("d"))
+      .ticks (30)
+      .tickPadding(8);
 
-    var line = d3.svg.line()
-      .x(function(d,i) { return xScale(+d.Year); })
-      .y(function(d) { return yScale(+d.LeaguePosition); })
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("left")
+      .tickPadding(8);
 
-    g.append("svg:path").attr("d", line(data));
+    vis.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + yScale.range()[0] + ")")
+      .call(xAxis);
+
+    vis.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
   });
-};
+}
